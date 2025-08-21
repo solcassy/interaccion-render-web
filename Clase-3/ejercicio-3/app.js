282c4c3c09fb3f8c03a7e5110854f914567052d1
solcassy <msanchezcas@centro.edu.mx>
2025-08-21 14:27:42 -0600

    animacion

---
+++ Clase-3/ejercicio-3/app.js
@@ -0,0 +1,16 @@
+console.log("sesion 03. ejercicio 3");
+console.log(gsap);
+
+
+window.addEventListener("mousedown", function(eventData) {
+    gsap.to(
+        ".rectangulo", 
+        {
+            x: 500,
+            y:100,
+            duration: 5,
+            ease: "bounce.in",
+            onComplete: function() {
+        }
+    );
+});
