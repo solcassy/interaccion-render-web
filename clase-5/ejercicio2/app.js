256a9033c5a69071e5725ffd25750905f29d2a40
solcassy <msanchezcas@centro.edu.mx>
2025-09-04 14:22:35 -0600

    clase 5

---
+++ clase-5/ejercicio2/app.js
@@ -0,0 +1,63 @@
+console.log('sesion 05. ejercicio 1 geometrias');
+console.log(THREE);
+
+// configurar el canvas
+const canvas = document.getElementById('lienzo');
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+//elementos basicos
+
+
+// escena,camara,mesh,renderer
+
+
+
+//escena
+const scene = new THREE.Scene();
+
+//camara
+const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
+
+
+//mesh
+const geometry = new THREE.TorusKnotGeometry();
+const material = new THREE.MeshToonMaterial( 
+    {flatShading: true,
+    specular: "#ffffff",
+    shininess: 10,
+    color: "#5C95FF",
+} );
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+mesh.position.z = -5;
+
+
+//renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(canvas.width, canvas.height);
+
+renderer.render(scene, camera);
+function animate() {
+    requestAnimationFrame(animate);
+ 
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+ 
+    renderer.render(scene, camera);
+ }
+ animate();
+
+
+ const topLight = new THREE.PointLight("#FFFFff", 100, 100);
+topLight.position.y = 5;
+scene.add(topLight);
+
+const frontLight = new THREE.PointLight("#DC758F", 100, 100);
+frontLight.position.set(3,1,3);
+scene.add(frontLight);
+
+
---
+++ clase-5/ejercicio3/app.js
@@ -0,0 +1,92 @@
+console.log('sesion 05. ejercicio 1 geometrias');
+console.log(THREE);
+
+// configurar el canvas
+const canvas = document.getElementById('lienzo');
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+//elementos basicos
+
+
+// escena,camara,mesh,renderer
+
+
+
+//escena
+const scene = new THREE.Scene();
+
+//camara
+const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
+
+
+//mesh
+const geometry = new THREE.TorusKnotGeometry();
+const material = new THREE.MeshNormalMaterial( 
+    {flatShading: true,});
+//const mesh = new THREE.Mesh(geometry, material);
+//scene.add(mesh);
+//mesh.position.z = -5;
+
+
+
+const textureLoader = new THREE.TextureLoader();
+var matcapMaterial;
+var mesh;
+var matcapMap = textureLoader.load(
+  // Textura URL
+  './assets/matcap1.png',
+  // on Load callback
+  function (texture) {
+      matcapMaterial = new THREE.MeshMatcapMaterial( { matcap: texture } );
+      // Mesh.
+      mesh = new THREE.Mesh( geometry, matcapMaterial );
+      // 3. Poner objeto en la escena.
+      scene.add(mesh);
+      mesh.position.z = -8;
+      // 4. Activar animación.
+      animate();
+  },
+  // on Progress (no funciona por ahora)
+  undefined,
+  // on Error callback
+  function (error) { console.error("Algo salio mal con la textura,", error); }
+);
+
+
+
+
+
+
+//const textureLoader = new THREE.TextureLoader();
+var matcapMaterial;
+var mesh;
+
+var matcapTexture = textureLoader.load('./assets/matcap1.png', function(texture) {
+console.log(texture);
+    
+});
+
+
+//renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(canvas.width, canvas.height);
+
+renderer.render(scene, camera);
+function animate() {
+    requestAnimationFrame(animate);
+ 
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+ 
+    renderer.render(scene, camera);
+ }
+ //animate();
+
+
+
+
+
